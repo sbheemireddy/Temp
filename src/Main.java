68229e26ed24f7ca2a8e2df15122e0dd5e6716f4
srikanth <s9908817216@gmail.com>
2020-10-11 14:49:47 +0530

    this is second commit

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello");
+		System.out.println("hello world");
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		String response = given().log().all().queryParam("key", "qaclick123").header("Content-type","application/json")
 		.body(Payloads.addPlace()).when().post("/maps/api/place/add/json")
