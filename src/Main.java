4b42d2b70af5a8f7b7f146b5bbd765c1a45af1be
srikanth <s9908817216@gmail.com>
2020-10-11 14:32:53 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import io.restassured.RestAssured.*;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payloads;
+import io.restassured.RestAssured;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello");
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-type","application/json")
+		.body(Payloads.addPlace()).when().post("/maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
+		System.out.println(response);
+		
+		JsonPath js = new JsonPath(response);
+		System.out.println("-------------"+js.getString("place_id"));
+		
+		
+		
+	}
+
+}
